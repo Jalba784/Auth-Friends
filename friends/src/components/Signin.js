39a45ba2431ea054d0835472f14a18de6dbc08eb
josepaniagua <jpanjr@gmail.com>
2020-03-14 00:28:58 -0700

    added imputs

--- friends/src/components/Signin.js
+++ friends/src/components/Signin.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signin = props => {
-    const { } = props;
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <div>
             <form>
-                <input type="email" name="email" placeholder="Email"/>
-                <input type="password" name="password" placeholder="Password"/>
+                <input type="email" name="email" placeholder="Email" value={data.email} onChange={handleChange}/>
+                <input type="password" name="password" placeholder="Password" value={data.password} onChange={handleChange}/>
 
                 <button type="submit">Sign In</button>
             </form>
